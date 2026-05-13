b8cb1f5086e574ad9fb249fd5d5d22101730f779
codeccv4-arch <codeccv4@gmail.com>
2026-05-13 22:25:40 +0700

    Update Speed.lua

--- Modules/Speed.lua
+++ Modules/Speed.lua
@@ -1,1 +1,13 @@
+local module = {}
 
+function module.Init(player, config)
+	local char = player.Character
+	if not char then return end
+
+	local hum = char:FindFirstChild("Humanoid")
+	if hum then
+		hum.WalkSpeed = config and config.walkspeed or 50
+	end
+end
+
+return module
